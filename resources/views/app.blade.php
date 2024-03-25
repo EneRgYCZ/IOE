44cf20385ca2a9f17dc2b7916d82aa263572a43d
Catalin Zaharia <32387669+EneRgYCZ@users.noreply.github.com>
2024-03-25 11:54:08 +0100

    Add CI  (#2)
    * Backend static and style
    
    * Backend static and style
    
    * Add CI pipeline and workflows
    
    * WIP

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
