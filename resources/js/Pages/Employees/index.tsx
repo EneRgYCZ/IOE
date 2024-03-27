7086576713d1b74899ab98375060fae5fecb86a0
Luca Colli <35486975+luca31@users.noreply.github.com>
2024-03-27 15:30:14 +0100

    Feature/employee team pages (#82)
    * Added table and floating action buttons to Employees and Teams
    
    * Remove unused parameter
    
    * Code style fix

--- resources/js/Pages/Employees/index.tsx
+++ resources/js/Pages/Employees/index.tsx
@@ -1,13 +1,40 @@
 import GuestLayout from "@/Layouts/GuestLayout";
-import { Employee, PageProps, PaginatedResponse } from "@/types";
+import { PageProps, PaginatedResponse, Employee } from "@/types";
 import React from "react";
+import { Button, Card, Fab, TableCell } from "@mui/material";
+import { Table } from "@/Components/table/table";
 
 const Employees = ({ employees }: PageProps<{ employees: PaginatedResponse<Employee> }>) => {
-    console.log(employees);
+    const tableButtonMargins = {
+        marginRight: "10px"
+    };
+
+    const addButtonStyle = {
+        position: "fixed",
+        bottom: 16,
+        right: 16
+    };
 
     return (
         <GuestLayout>
-            <h1>Employees</h1>
+            <Card variant="outlined" sx={{ width: "70%", alignItems: "center" }}>
+                <Table<Employee>
+                    data={employees}
+                    actionRenderer={() => (
+                        <TableCell align="center">
+                            <Button variant="contained" sx={tableButtonMargins}>
+                                EDIT
+                            </Button>
+                            <Button variant="contained" color="error">
+                                DELETE
+                            </Button>
+                        </TableCell>
+                    )}
+                />
+            </Card>
+            <Fab variant="extended" color="primary" sx={addButtonStyle}>
+                Add employee
+            </Fab>
         </GuestLayout>
     );
 };
--- resources/js/Pages/Teams/index.tsx
+++ resources/js/Pages/Teams/index.tsx
@@ -1,13 +1,41 @@
 import GuestLayout from "@/Layouts/GuestLayout";
 import { PageProps, PaginatedResponse, Team } from "@/types";
 import React from "react";
+import { Button, Card, Fab, TableCell } from "@mui/material";
+import { Table } from "@/Components/table/table";
 
 const Teams = ({ teams }: PageProps<{ teams: PaginatedResponse<Team> }>) => {
-    console.log(teams);
+    const tableButtonMargins = {
+        margin: "0 10px"
+    };
+
+    const addButtonStyle = {
+        position: "fixed",
+        bottom: 16,
+        right: 16
+    };
 
     return (
         <GuestLayout>
-            <h1>Teams</h1>
+            <Card variant="outlined" sx={{ width: "70%", alignItems: "center" }}>
+                <Table<Team>
+                    data={teams}
+                    actionRenderer={() => (
+                        <TableCell align="center">
+                            <Button variant="contained">VIEW</Button>
+                            <Button variant="contained" sx={tableButtonMargins}>
+                                EDIT
+                            </Button>
+                            <Button variant="contained" color="error">
+                                DELETE
+                            </Button>
+                        </TableCell>
+                    )}
+                />
+            </Card>
+            <Fab variant="extended" color="primary" sx={addButtonStyle}>
+                Add team
+            </Fab>
         </GuestLayout>
     );
 };
