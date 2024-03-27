29b70e12ca95560bbd0d726b0cf4de0b420703f1
luca <lucacolli5@gmail.com>
2024-03-27 12:22:41 +0100

    Code style fix

--- resources/js/Pages/Employees/index.tsx
+++ resources/js/Pages/Employees/index.tsx
@@ -1,37 +1,35 @@
 import GuestLayout from "@/Layouts/GuestLayout";
-import {PageProps, PaginatedResponse, Employee} from "@/types";
+import { PageProps, PaginatedResponse, Employee } from "@/types";
 import React from "react";
-import {Button, Card, Fab, TableCell} from "@mui/material";
-import {Table} from "@/Components/table/table";
+import { Button, Card, Fab, TableCell } from "@mui/material";
+import { Table } from "@/Components/table/table";
 
 const Employees = ({ employees }: PageProps<{ employees: PaginatedResponse<Employee> }>) => {
     const tableButtonMargins = {
         marginRight: "10px"
-    }
+    };
 
     const addButtonStyle = {
-        position: 'fixed',
+        position: "fixed",
         bottom: 16,
-        right: 16,
+        right: 16
     };
 
     return (
         <GuestLayout>
             <Card variant="outlined" sx={{ width: "70%", alignItems: "center" }}>
-                <Table <Employee>
+                <Table<Employee>
                     data={employees}
-                    actionRenderer={() =>
-                        (
-                            <TableCell align="center">
-                                <Button variant="contained" sx={tableButtonMargins}>
-                                    EDIT
-                                </Button>
-                                <Button variant="contained" color="error">
-                                    DELETE
-                                </Button>
-                            </TableCell>
-                        )
-                    }
+                    actionRenderer={() => (
+                        <TableCell align="center">
+                            <Button variant="contained" sx={tableButtonMargins}>
+                                EDIT
+                            </Button>
+                            <Button variant="contained" color="error">
+                                DELETE
+                            </Button>
+                        </TableCell>
+                    )}
                 />
             </Card>
             <Fab variant="extended" color="primary" sx={addButtonStyle}>
--- resources/js/Pages/Teams/index.tsx
+++ resources/js/Pages/Teams/index.tsx
@@ -1,40 +1,36 @@
 import GuestLayout from "@/Layouts/GuestLayout";
-import {PageProps, PaginatedResponse, Team} from "@/types";
+import { PageProps, PaginatedResponse, Team } from "@/types";
 import React from "react";
-import {Button, Card, Fab, TableCell} from "@mui/material";
-import {Table} from "@/Components/table/table";
+import { Button, Card, Fab, TableCell } from "@mui/material";
+import { Table } from "@/Components/table/table";
 
 const Teams = ({ teams }: PageProps<{ teams: PaginatedResponse<Team> }>) => {
     const tableButtonMargins = {
         margin: "0 10px"
-    }
+    };
 
     const addButtonStyle = {
-        position: 'fixed',
+        position: "fixed",
         bottom: 16,
-        right: 16,
+        right: 16
     };
 
     return (
         <GuestLayout>
             <Card variant="outlined" sx={{ width: "70%", alignItems: "center" }}>
-                <Table <Team>
+                <Table<Team>
                     data={teams}
-                    actionRenderer={() =>
-                        (
-                            <TableCell align="center">
-                                <Button variant="contained">
-                                    VIEW
-                                </Button>
-                                <Button variant="contained" sx={tableButtonMargins}>
-                                    EDIT
-                                </Button>
-                                <Button variant="contained" color="error">
-                                    DELETE
-                                </Button>
-                            </TableCell>
-                        )
-                    }
+                    actionRenderer={() => (
+                        <TableCell align="center">
+                            <Button variant="contained">VIEW</Button>
+                            <Button variant="contained" sx={tableButtonMargins}>
+                                EDIT
+                            </Button>
+                            <Button variant="contained" color="error">
+                                DELETE
+                            </Button>
+                        </TableCell>
+                    )}
                 />
             </Card>
             <Fab variant="extended" color="primary" sx={addButtonStyle}>
