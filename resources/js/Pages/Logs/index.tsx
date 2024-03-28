d78c2591fa57123be6f4f9bdfe315759dd86748f
Aurelie Gallet <a.b.j.gallet@student.rug.nl>
2024-03-28 16:25:26 +0100

    Of course I forgot the logs...

--- resources/js/Pages/Logs/index.tsx
+++ resources/js/Pages/Logs/index.tsx
@@ -1,15 +1,14 @@
 import GuestLayout from "@/Layouts/GuestLayout";
-import { PageProps, PaginatedResponse, Logs} from "@/types";
+import { PageProps, PaginatedResponse, Logs } from "@/types";
 import React from "react";
 import { Card } from "@mui/material";
 import { Table } from "@/Components/table/table";
-
 
 const Log = ({ logs }: PageProps<{ logs: PaginatedResponse<Logs> }>) => {
     return (
         <GuestLayout>
             <Card variant="outlined" sx={{ width: "70%", alignItems: "center" }}>
-                <Table<Logs> data={logs}/>
+                <Table<Logs> data={logs} />
             </Card>
         </GuestLayout>
     );
