2ea5c1017cc50207984fe6cc80d34d7a6fbad842
Rishabh John <johnrishabhalex@gmail.com>
2024-04-23 20:21:35 +0200

    Deletion of Employee (Draft)

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Employee;
 use App\Table\Column;
 use App\Table\SearchInput;
+use Illuminate\Http\Request;
 use App\Table\Table;
 use Inertia\Inertia;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -34,5 +35,48 @@ class EmployeeController extends Controller
                 ->addSearchInput(new SearchInput('first_name', 'First Name', shown: true))
                 ->addSearchInput(new SearchInput('last_name', 'Last Name', shown: true));
         });
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->create();
+        return redirect(route('employee.index'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employee $employee)
+    {
+        $employee->delete();
+        return redirect(route('employee.index'));
     }
 }
--- resources/js/Pages/Employees/index.tsx
+++ resources/js/Pages/Employees/index.tsx
@@ -3,6 +3,9 @@ import { PageProps, PaginatedResponse, Employee } from "@/types";
 import React from "react";
 import { Button, Card, Fab, TableCell } from "@mui/material";
 import { Table } from "@/Components/table/table";
+import {Link} from "@inertiajs/react";
+import { link } from "fs";
+
 
 const Employees = ({ employees }: PageProps<{ employees: PaginatedResponse<Employee> }>) => {
     const tableButtonMargins = {
@@ -15,24 +18,34 @@ const Employees = ({ employees }: PageProps<{ employees: PaginatedResponse<Emplo
         right: 16
     };
 
+    // const handleClick=(employee: { id: any; })=> {
+    //     console.log("Button Clicked")
+    //     console.log(employee.id)
+        
+    // }
+
     return (
         <GuestLayout>
             <Card variant="outlined" sx={{ width: "70%", alignItems: "center" }}>
                 <Table<Employee>
+                    
                     data={employees}
-                    actionRenderer={() => (
+                    actionRenderer={(employee: { id: any; }) => (
                         <TableCell align="center">
                             <Button variant="contained" sx={tableButtonMargins}>
                                 EDIT
                             </Button>
+                            <Link href={route("employee.destroy", employee.id)} method="delete">
                             <Button variant="contained" color="error">
                                 DELETE
                             </Button>
+                            </Link>
+                           
                         </TableCell>
                     )}
                 />
             </Card>
-            <Fab variant="extended" color="primary" sx={addButtonStyle}>
+            <Fab variant="extended" color="primary" sx={addButtonStyle} >
                 Add employee
             </Fab>
         </GuestLayout>
