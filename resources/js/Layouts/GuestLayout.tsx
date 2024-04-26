48c1669334ef89923fccf5f87e9a40e50e038ba6
Aurelie Gallet <a.b.j.gallet@student.rug.nl>
2024-04-26 10:32:19 +0200

    Navigation abr stays fixed

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -6,9 +6,7 @@ import NavigationBar from "@/Components/navigation/navigation-bar";
 export default function GuestLayout({ children }: PropsWithChildren) {
     return (
         <>
-            <Box sx={{ width: "100%", marginBottom: "40px" }}>
-                <NavigationBar />
-            </Box>
+            <NavigationBar />
             <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" maxWidth="100vw">
                 {children}
             </Box>
