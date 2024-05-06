0a6d688d84c523bdf852e7075aeaa91d3cce0bd7
Catalin Zaharia <32387669+EneRgYCZ@users.noreply.github.com>
2024-05-06 13:24:54 +0200

    Split equipment in 3 pages (#109)
    * Split equipment in 3 pages
    
    * In progress
    
    * Style
    
    * Buttons removed

--- routes/web.php
+++ routes/web.php
@@ -33,15 +33,24 @@ Route::group(['prefix' => 'employees', 'as' => 'employees.'], function () {
 });
 
 Route::group(['prefix' => 'equipment', 'as' => 'equipment.'], function () {
-    Route::get('/', [\App\Http\Controllers\EquipmentController::class, 'index'])->name('index');
+    // Add view routes for each category
+    Route::get('/desktops', [\App\Http\Controllers\EquipmentController::class, 'desktopsIndex'])->name('desktops');
+    Route::get('/laptops', [\App\Http\Controllers\EquipmentController::class, 'laptopsIndex'])->name('laptops');
+    Route::get('/meeting-room-laptops', [\App\Http\Controllers\EquipmentController::class, 'meetingRoomLaptopIndex'])->name('meeting-room-laptops');
+
+    // CRUD operations for desktops
     Route::post('/desktop', [\App\Http\Controllers\EquipmentController::class, 'storeDesktop'])->name('storeDesktop');
-    Route::post('/laptop', [\App\Http\Controllers\EquipmentController::class, 'storeLaptop'])->name('storeLaptop');
-    Route::post('/meetingRoomLaptop', [\App\Http\Controllers\EquipmentController::class, 'storeMeetingRoomLaptop'])->name('storeMeetingRoomLaptop');
     Route::patch('/desktop/{desktop}', [\App\Http\Controllers\EquipmentController::class, 'updateDesktop'])->name('updateDesktop');
-    Route::patch('/laptop/{laptop}', [\App\Http\Controllers\EquipmentController::class, 'updateLaptop'])->name('updateLaptop');
-    Route::patch('/meetingRoomLaptop/{meetingRoomLaptop}', [\App\Http\Controllers\EquipmentController::class, 'updateMeetingRoomLaptop'])->name('updateMeetingRoomLaptop');
     Route::delete('/desktop/{desktop}', [\App\Http\Controllers\EquipmentController::class, 'destroyDesktop'])->name('destroyDesktop');
+
+    // CRUD operations for laptops
+    Route::post('/laptop', [\App\Http\Controllers\EquipmentController::class, 'storeLaptop'])->name('storeLaptop');
+    Route::patch('/laptop/{laptop}', [\App\Http\Controllers\EquipmentController::class, 'updateLaptop'])->name('updateLaptop');
     Route::delete('/laptop/{laptop}', [\App\Http\Controllers\EquipmentController::class, 'destroyLaptop'])->name('destroyLaptop');
+
+    // CRUD operations for meeting room laptops
+    Route::post('/meetingRoomLaptop', [\App\Http\Controllers\EquipmentController::class, 'storeMeetingRoomLaptop'])->name('storeMeetingRoomLaptop');
+    Route::patch('/meetingRoomLaptop/{meetingRoomLaptop}', [\App\Http\Controllers\EquipmentController::class, 'updateMeetingRoomLaptop'])->name('updateMeetingRoomLaptop');
     Route::delete('/meetingRoomLaptop/{meetingRoomLaptop}', [\App\Http\Controllers\EquipmentController::class, 'destroyMeetingRoomLaptop'])->name('destroyMeetingRoomLaptop');
 });
 
