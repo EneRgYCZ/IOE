d5c12d60fca3189540e0459ff81b14ee83d2167c
Catalin Zaharia <catalin.zaharia2013@gmail.com>
2024-05-12 16:42:12 +0200

    Equipment

---
+++ tests/Unit/Equipment/DesktopTest.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace Tests\Unit\Equipment;
+
+use App\Models\Desktop;
+
+it('can create a desktop', function () {
+    $desktopData = [
+        'full_number_identifier' => '12345',
+        'pc_number' => 'PC001',
+        'location' => 'ghh',
+        'side' => 'north',
+        'double_pc' => false,
+        'needs_dock' => true,
+        'status' => 'static',
+        'floor' => 1,
+        'island_number' => 101,
+        'workspace_type' => 'developer',
+        'updated_in_q1' => false,
+    ];
+
+    $response = $this->post(route('equipment.storeDesktop'), $desktopData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $desktopData);
+});
+
+it('can display desktops', function () {
+    Desktop::factory()->count(3)->create();
+
+    $response = $this->get(route('equipment.desktops'));
+
+    $response->assertStatus(200)
+        ->assertInertia(
+            fn ($page) => $page
+                ->component('Equipment/Desktop/index')
+                ->has('desktops.data', 3)
+                ->has(
+                    'desktops.data.0',
+                    fn ($page) => $page
+                        ->where('id', fn ($value) => is_numeric($value))
+                        ->where('full_number_identifier', fn ($value) => is_string($value))
+                        ->where('pc_number', fn ($value) => is_string($value))
+                        ->etc()
+                )
+        );
+});
+
+it('can update the full number identifier of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => '54321',
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the PC number of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => 'PC002',
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the location of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location === 'ghh' ? 'waagstraat' : 'ghh',
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the side of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => 'south',
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the floor of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => 2,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the island number of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => 102,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the double PC flag of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => !$desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the needs dock flag of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => !$desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the workspace type of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type === 'developer' ? 'non-developer' : 'developer',
+        'updated_in_q1' => $desktop->updated_in_q1,
+    ];
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can update the updated in Q1 flag of a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $desktop->full_number_identifier,
+        'pc_number' => $desktop->pc_number,
+        'location' => $desktop->location,
+        'side' => $desktop->side,
+        'double_pc' => $desktop->double_pc,
+        'needs_dock' => $desktop->needs_dock,
+        'status' => $desktop->status,
+        'floor' => $desktop->floor,
+        'island_number' => $desktop->island_number,
+        'workspace_type' => $desktop->workspace_type,
+        'updated_in_q1' => !$desktop->updated_in_q1,
+    ];
+    $response = $this->patch(route('equipment.updateDesktop', $desktop), $updatedData);
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseHas('desktops', $updatedData);
+});
+
+it('can delete a desktop', function () {
+    $desktop = Desktop::factory()->create();
+    $response = $this->delete(route('equipment.destroyDesktop', $desktop));
+    $response->assertRedirect(route('equipment.desktops'));
+    $this->assertDatabaseMissing('desktops', ['id' => $desktop->id]);
+});
---
+++ tests/Unit/Equipment/MeetingRoomLaptopTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Tests\Unit\Equipment;
+
+use App\Models\MeetingRoomLaptop;
+
+it('can create a meeting room laptop', function () {
+    $meetingRoomLaptopData = [
+        'full_number_identifier' => 'MR123',
+        'laptop_number' => 'MR001',
+        'location' => 'waagstraat',
+        'side' => 'south',
+        'floor' => 3,
+        'room_number' => '300A',
+        'updated_in_q1' => false,
+    ];
+
+    $response = $this->post(route('equipment.storeMeetingRoomLaptop'), $meetingRoomLaptopData);
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseHas('meeting_room_laptops', $meetingRoomLaptopData);
+});
+
+it('can display meeting room laptops', function () {
+    MeetingRoomLaptop::factory()->count(3)->create();
+
+    $response = $this->get(route('equipment.meeting-room-laptops'));
+
+    $response->assertStatus(200)
+        ->assertInertia(
+            fn ($page) => $page
+                ->component('Equipment/MeetingRoomLaptop/index')
+                ->has('meetingRoomLaptops.data', 3)
+                ->has(
+                    'meetingRoomLaptops.data.0',
+                    fn ($page) => $page
+                        ->where('id', fn ($value) => is_numeric($value))
+                        ->where('full_number_identifier', fn ($value) => is_string($value))
+                        ->where('laptop_number', fn ($value) => is_string($value))
+                        ->etc()
+                )
+        );
+});
+
+
+it('can update the full number identifier of a meeting room laptop', function () {
+    $meetingRoomLaptop = MeetingRoomLaptop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => 'MR456',
+        'laptop_number' => $meetingRoomLaptop->laptop_number,
+        'location' => $meetingRoomLaptop->location,
+        'side' => $meetingRoomLaptop->side,
+        'floor' => $meetingRoomLaptop->floor,
+        'room_number' => $meetingRoomLaptop->room_number,
+        'updated_in_q1' => $meetingRoomLaptop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateMeetingRoomLaptop', $meetingRoomLaptop), $updatedData);
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseHas('meeting_room_laptops', $updatedData);
+});
+
+it('can update the laptop number of a meeting room laptop', function () {
+    $meetingRoomLaptop = MeetingRoomLaptop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $meetingRoomLaptop->full_number_identifier,
+        'laptop_number' => 'MR002',
+        'location' => $meetingRoomLaptop->location,
+        'side' => $meetingRoomLaptop->side,
+        'floor' => $meetingRoomLaptop->floor,
+        'room_number' => $meetingRoomLaptop->room_number,
+        'updated_in_q1' => $meetingRoomLaptop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateMeetingRoomLaptop', $meetingRoomLaptop), $updatedData);
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseHas('meeting_room_laptops', $updatedData);
+});
+
+it('can update the location of a meeting room laptop', function () {
+    $meetingRoomLaptop = MeetingRoomLaptop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $meetingRoomLaptop->full_number_identifier,
+        'laptop_number' => $meetingRoomLaptop->laptop_number,
+        'location' => $meetingRoomLaptop->location === 'ghh' ? 'waagstraat' : 'ghh',
+        'side' => $meetingRoomLaptop->side,
+        'floor' => $meetingRoomLaptop->floor,
+        'room_number' => $meetingRoomLaptop->room_number,
+        'updated_in_q1' => $meetingRoomLaptop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateMeetingRoomLaptop', $meetingRoomLaptop), $updatedData);
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseHas('meeting_room_laptops', $updatedData);
+});
+
+it('can update the side of a meeting room laptop', function () {
+    $meetingRoomLaptop = MeetingRoomLaptop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $meetingRoomLaptop->full_number_identifier,
+        'laptop_number' => $meetingRoomLaptop->laptop_number,
+        'location' => $meetingRoomLaptop->location,
+        'side' => 'north',
+        'floor' => $meetingRoomLaptop->floor,
+        'room_number' => $meetingRoomLaptop->room_number,
+        'updated_in_q1' => $meetingRoomLaptop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateMeetingRoomLaptop', $meetingRoomLaptop), $updatedData);
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseHas('meeting_room_laptops', $updatedData);
+});
+
+it('can update the floor of a meeting room laptop', function () {
+    $meetingRoomLaptop = MeetingRoomLaptop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $meetingRoomLaptop->full_number_identifier,
+        'laptop_number' => $meetingRoomLaptop->laptop_number,
+        'location' => $meetingRoomLaptop->location,
+        'side' => $meetingRoomLaptop->side,
+        'floor' => 4,
+        'room_number' => $meetingRoomLaptop->room_number,
+        'updated_in_q1' => $meetingRoomLaptop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateMeetingRoomLaptop', $meetingRoomLaptop), $updatedData);
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseHas('meeting_room_laptops', $updatedData);
+});
+
+it('can update the room number of a meeting room laptop', function () {
+    $meetingRoomLaptop = MeetingRoomLaptop::factory()->create();
+    $updatedData = [
+        'full_number_identifier' => $meetingRoomLaptop->full_number_identifier,
+        'laptop_number' => $meetingRoomLaptop->laptop_number,
+        'location' => $meetingRoomLaptop->location,
+        'side' => $meetingRoomLaptop->side,
+        'floor' => $meetingRoomLaptop->floor,
+        'room_number' => '302A',
+        'updated_in_q1' => $meetingRoomLaptop->updated_in_q1,
+    ];
+
+    $response = $this->patch(route('equipment.updateMeetingRoomLaptop', $meetingRoomLaptop), $updatedData);
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseHas('meeting_room_laptops', $updatedData);
+});
+
+
+it('can delete a meeting room laptop', function () {
+    $meetingRoomLaptop = MeetingRoomLaptop::factory()->create();
+    $response = $this->delete(route('equipment.destroyMeetingRoomLaptop', $meetingRoomLaptop));
+    $response->assertRedirect(route('equipment.meeting-room-laptops'));
+    $this->assertDatabaseMissing('meeting_room_laptops', ['id' => $meetingRoomLaptop->id]);
+});
