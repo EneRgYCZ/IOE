1c7fb15c3a2c1c8ead688d7363db78d74d89bddc
luca <lucacolli5@gmail.com>
2024-05-12 21:02:31 +0200

    Separate drawer with filters

--- resources/js/Components/table/column-toggler.tsx
+++ resources/js/Components/table/column-toggler.tsx
@@ -1,31 +1,28 @@
 import { QueryBuilderColumn } from "@/types";
 
-import React, { useRef } from "react";
+import React from "react";
 import { useState } from "react";
-import { HiOutlineEyeSlash } from "react-icons/hi2";
 
-import { FormControlLabel, IconButton, Menu, Switch, Typography } from "@mui/material";
+import { FormControlLabel, Switch, Typography } from "@mui/material";
 import { MenuItem } from "@mui/material";
 
 const ColumnToggler: React.FC<{
     columns: Array<QueryBuilderColumn>;
     columnToggledHandler: (column: QueryBuilderColumn, state: boolean) => void;
 }> = ({ columns, columnToggledHandler }) => {
-    const hasHiddenColumns = columns.some(column => column.hidden);
-
     const IndividualToggle: React.FC<{
         column: QueryBuilderColumn;
     }> = ({ column }) => {
-        const [hidden, setHidden] = useState(column.hidden);
+        const [shown, setShown] = useState(!column.hidden);
 
         return (
             <FormControlLabel
                 control={
                     <Switch
-                        checked={hidden}
+                        checked={shown}
                         onChange={e => {
-                            setHidden(e.target.checked);
-                            columnToggledHandler(column, e.target.checked);
+                            setShown(e.target.checked);
+                            columnToggledHandler(column, !e.target.checked);
                         }}
                         color="primary"
                     />
@@ -45,41 +42,13 @@ const ColumnToggler: React.FC<{
         );
     };
 
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const togglerRef = useRef(null);
-
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
     return (
         <>
-            <IconButton
-                ref={togglerRef}
-                color={hasHiddenColumns ? "primary" : "default"}
-                onClick={handleClick}
-                sx={{
-                    border: 1,
-                    borderColor: "divider",
-                    borderRadius: 1,
-                    padding: 1
-                }}
-            >
-                <HiOutlineEyeSlash />
-            </IconButton>
-
-            <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                {columns.map(col => (
-                    <MenuItem key={col.key}>
-                        <IndividualToggle column={col} />
-                    </MenuItem>
-                ))}
-            </Menu>
+            {columns.map(col => (
+                <MenuItem key={col.key}>
+                    <IndividualToggle column={col} />
+                </MenuItem>
+            ))}
         </>
     );
 };
