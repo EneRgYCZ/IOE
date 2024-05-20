a412dc0d90cc406d42642d22b8922ef263f5c4e6
Rishabh John <johnrishabhalex@gmail.com>
2024-05-20 14:44:41 +0200

    More Tests

--- tests/Unit/Employees/EmployeesTest.php
+++ tests/Unit/Employees/EmployeesTest.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Models\Employee;
+use App\Models\Team;
+use App\Models\TeamMember;
+use App\Models\Desktop;
+use App\Models\Laptop;
 
 it('can create an employee', function () {
     $employeeData = [
@@ -59,6 +63,162 @@ it('can search for an employee', function () {
     $response->assertSee('testFirstName');
 });
 
+it('can assign an employee to a team', function () {
+    $employeeData = [
+        'first_name' => 'testFirstName',
+        'last_name' => 'testLastName',
+    ];
+    $employee = Employee::create($employeeData);
+
+    $teamData = [
+        'team_name' => 'Test Team',
+        'description' => 'This is a test team',
+        'team_members' => [
+            ['id' => $employee->id],
+        ],
+    ];
+    $team = Team::create($teamData);
+    $teamMemberData = [
+        'team_id' => $team->id,
+        'employee_id' => $employee->id,
+    ];
+    $teamMember = TeamMember::create($teamMemberData);
+
+    $this->assertEquals($team->id, $teamMember->team_id);
+    $this->assertEquals($employee->id, $teamMember->employee_id);
+
+});
+
+it('can unassign an employee from a team', function () {
+    $employeeData = [
+        'first_name' => 'testFirstName',
+        'last_name' => 'testLastName',
+    ];
+    $employee = Employee::create($employeeData);
+
+    $teamData = [
+        'team_name' => 'Test Team',
+        'description' => 'This is a test team',
+        'team_members' => [
+            ['id' => $employee->id],
+        ],
+    ];
+    $team = Team::create($teamData);
+    $teamMemberData = [
+        'team_id' => $team->id,
+        'employee_id' => $employee->id,
+    ];
+    $teamMember = TeamMember::create($teamMemberData);
+
+    $this->assertEquals($team->id, $teamMember->team_id);
+    $this->assertEquals($employee->id, $teamMember->employee_id);
+
+    $teamMember->delete();
+    $this->assertNull(TeamMember::where('team_id', $team->id)->where('employee_id', $employee->id)->first());
+
+});
+
+
+it('can assign desktop to an employee', function () {
+    $employeeData = [
+        'first_name' => 'Test',
+        'last_name' => 'Employee',
+    ];
+    $employee = Employee::create($employeeData);
+    $desktopData = [
+        'full_number_identifier' => '12345',
+        'pc_number' => 'PC001',
+        'location' => 'ghh',
+        'side' => 'north',
+        'double_pc' => false,
+        'needs_dock' => true,
+        'status' => 'static',
+        'floor' => 1,
+        'island_number' => 101,
+        'workspace_type' => 'developer',
+        'updated_in_q1' => false,
+    ];
+    $desktop = Desktop::create($desktopData);
+    $employee->equipment_id = $desktop->id;
+
+    $this->assertEquals($desktop->id, $employee->equipment_id);
+});
+
+it('can unassign desktop to an employee', function () {
+    $employeeData = [
+        'first_name' => 'Test',
+        'last_name' => 'Employee',
+    ];
+    $employee = Employee::create($employeeData);
+    $desktopData = [
+        'full_number_identifier' => '12345',
+        'pc_number' => 'PC001',
+        'location' => 'ghh',
+        'side' => 'north',
+        'double_pc' => false,
+        'needs_dock' => true,
+        'status' => 'static',
+        'floor' => 1,
+        'island_number' => 101,
+        'workspace_type' => 'developer',
+        'updated_in_q1' => false,
+    ];
+    $desktop = Desktop::create($desktopData);
+    $employee->equipment_id = $desktop->id;
+
+    $this->assertEquals($desktop->id, $employee->equipment_id);
+    $employee->equipment_id = null;
+    $this->assertNull($employee->equipment_id);
+});
+
+it('can assign laptop to an employee', function () {
+    $employeeData = [
+        'first_name' => 'Test',
+        'last_name' => 'Employee',
+    ];
+    $employee = Employee::create($employeeData);
+    $laptopData = [
+        'full_number_identifier' => 'L12345',
+        'laptop_number' => 'L001',
+        'location' => 'ghh',
+        'side' => 'north',
+        'status' => 'static',
+        'floor' => 2,
+        'island_number' => 102,
+        'workspace_type' => 'non-developer',
+        'updated_in_q1' => true,
+    ];
+
+    $laptop = Laptop::create($laptopData);
+    $employee->equipment_id = $laptop->id;
+
+    $this->assertEquals($laptop->id, $employee->equipment_id);
+});
+it('can unassign laptop to an employee', function () {
+    $employeeData = [
+        'first_name' => 'Test',
+        'last_name' => 'Employee',
+    ];
+    $employee = Employee::create($employeeData);
+    $laptopData = [
+        'full_number_identifier' => 'L12345',
+        'laptop_number' => 'L001',
+        'location' => 'ghh',
+        'side' => 'north',
+        'status' => 'static',
+        'floor' => 2,
+        'island_number' => 102,
+        'workspace_type' => 'non-developer',
+        'updated_in_q1' => true,
+    ];
+
+    $laptop = Laptop::create($laptopData);
+    $employee->equipment_id = $laptop->id;
+
+    $this->assertEquals($laptop->id, $employee->equipment_id);
+    $employee->equipment_id = null;
+    $this->assertNull($employee->equipment_id);
+});
 it('can sort employees by first name in ascending order', function () {
     $employeeData = [
         'first_name' => 'Abby',
