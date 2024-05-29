d20bb9d9b51f5a1c68b29576b77987b4cac13b5c
Catalin Zaharia <catalin.zaharia2013@gmail.com>
2024-05-29 21:32:49 +0200

    Toast type

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\ToastType;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,30 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function toast(
+        string $title,
+        ToastType $type,
+        string $description = '',
+        int $ms = 5000,
+        array $action = null
+    ): void {
+        request()?->session()->flash('toast', self::buildToast($title, $type, $description, $ms, $action));
+    }
+
+    public static function buildToast(
+        string $title,
+        ToastType $type,
+        string $description = '',
+        int $ms = 5000,
+        array $action = null
+    ): array {
+        return [
+            'title' => $title,
+            'description' => $description,
+            'type' => $type,
+            'timeout' => $ms,
+            'action' => $action,
+        ];
+    }
 }
